6a5085d0d66c3329a4c4ca666d2f126d947e7dfa
xukui <xukui@wondersgroup.com>
2019-06-27 09:49:33 +0800

    init

--- library/src/main/java/com/allenliu/versionchecklib/core/DownloadManager.java
+++ library/src/main/java/com/allenliu/versionchecklib/core/DownloadManager.java
@@ -1,6 +1,5 @@
 package com.allenliu.versionchecklib.core;
 
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -19,7 +18,6 @@ import com.allenliu.versionchecklib.R;
 import com.allenliu.versionchecklib.callback.DownloadListener;
 import com.allenliu.versionchecklib.core.http.AllenHttp;
 import com.allenliu.versionchecklib.core.http.FileCallBack;
-import com.allenliu.versionchecklib.utils.ALog;
 import com.allenliu.versionchecklib.utils.AppUtils;
 
 import java.io.File;
@@ -31,24 +29,19 @@ import okhttp3.Response;
 import static android.app.PendingIntent.FLAG_UPDATE_CURRENT;
 import static android.content.Context.NOTIFICATION_SERVICE;
 
-//import android.support.v4.app.NotificationCompat;
-//import android.support.v7.app.NotificationCompat;
-
-/**
- * Created by allenliu on 2017/8/16.
- */
-
 public class DownloadManager {
+
     private static int lastProgress = 0;
     private static boolean isDownloadSuccess = false;
 
-    //   private static final int TASK=Intent.FLAG_ACTIVITY_CLEAR_TOP;
     public static void downloadAPK(final String url, final VersionParams versionParams, final DownloadListener listener) {
         lastProgress = 0;
         isDownloadSuccess = false;
+
         if (url == null || url.isEmpty()) {
             return;
         }
+
         String downloadPath = versionParams.getDownloadAPKPath() + AllenChecker.getGlobalContext().getString(R.string.versionchecklib_download_apkname, AllenChecker.getGlobalContext().getPackageName());
         //静默下载也判断本地是否有缓存
         if (versionParams.isSilentDownload()) {
@@ -61,8 +54,9 @@ public class DownloadManager {
                 }
                 silentDownloadAPK(AllenChecker.getGlobalContext(), url, versionParams, listener);
 
-            } else
+            } else {
                 silentDownloadAPK(AllenChecker.getGlobalContext(), url, versionParams, listener);
+            }
             return;
         }
 
@@ -76,16 +70,15 @@ public class DownloadManager {
 
             }
         }
-        if (listener != null)
+        if (listener != null) {
             listener.onCheckerStartDownload();
+        }
         NotificationCompat.Builder builder = null;
         NotificationManager manager = null;
-        Notification notification;
         if (versionParams.isShowNotification()) {
             manager = (NotificationManager) AllenChecker.getGlobalContext().getSystemService(NOTIFICATION_SERVICE);
             builder = createNotification(AllenChecker.getGlobalContext());
             manager.notify(0, builder.build());
-
         }
         final NotificationCompat.Builder finalBuilder = builder;
         final NotificationManager finalManager = manager;
@@ -97,18 +90,18 @@ public class DownloadManager {
                 isDownloadSuccess = true;
                 if (versionParams.isShowNotification()) {
                     Intent i = new Intent(Intent.ACTION_VIEW);
+
                     Uri uri;
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
                         uri = VersionFileProvider.getUriForFile(AllenChecker.getGlobalContext(), AllenChecker.getGlobalContext().getPackageName() + ".versionProvider", file);
-                        ALog.e(AllenChecker.getGlobalContext().getPackageName() + "");
                         i.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
                     } else {
                         uri = Uri.fromFile(file);
                     }
-                    ALog.e("APK download Success");
+
                     //设置intent的类型
-                    i.setDataAndType(uri,
-                            "application/vnd.android.package-archive");
+                    i.setDataAndType(uri, "application/vnd.android.package-archive");
                     PendingIntent pendingIntent = PendingIntent.getActivity(AllenChecker.getGlobalContext(), 0, i, 0);
                     finalBuilder.setContentIntent(pendingIntent);
                     finalBuilder.setContentText(AllenChecker.getGlobalContext().getString(R.string.versionchecklib_download_finish));
@@ -122,17 +115,15 @@ public class DownloadManager {
 
             @Override
             public void onDownloading(int progress) {
-                ALog.e("downloadProgress:" + progress + "");
                 int currentProgress = progress;
-//                showLoadingDialog(currentProgress);
                 listener.onCheckerDownloading(currentProgress);
+
                 if (currentProgress - lastProgress >= 5) {
                     lastProgress = currentProgress;
                     if (versionParams.isShowNotification() && !isDownloadSuccess) {
                         finalBuilder.setContentIntent(null);
                         finalBuilder.setContentText(String.format(AllenChecker.getGlobalContext().getString(R.string.versionchecklib_download_progress), lastProgress));
                         finalBuilder.setProgress(100, lastProgress, false);
-//                        finalBuilder.setDefaults(0);
                         finalManager.notify(0, finalBuilder.build());
                     }
                 }
@@ -143,7 +134,6 @@ public class DownloadManager {
                 if (versionParams.isShowNotification()) {
                     Intent intent = new Intent(AllenChecker.getGlobalContext(), versionParams.getCustomDownloadActivityClass());
                     intent.putExtra("isRetry", true);
-//                intent.putExtra(AVersionService.VERSION_PARAMS_KEY, paramBundle);
                     intent.putExtra(AVersionService.VERSION_PARAMS_KEY, versionParams);
                     intent.putExtra("downloadUrl", url);
                     PendingIntent pendingIntent = PendingIntent.getActivity(AllenChecker.getGlobalContext(), 0, intent, FLAG_UPDATE_CURRENT);
@@ -152,33 +142,27 @@ public class DownloadManager {
                     finalBuilder.setProgress(100, 0, false);
                     finalManager.notify(0, finalBuilder.build());
                 }
-                ALog.e("file download failed");
-//                showFailDialog();
                 listener.onCheckerDownloadFail();
-
             }
+
         });
     }
 
-
     private static void silentDownloadAPK(final Context context, String url, final VersionParams versionParams, final DownloadListener listener) {
         Request request = new Request.Builder().url(url).build();
-        if (listener != null)
+        if (listener != null) {
             listener.onCheckerStartDownload();
+        }
         AllenHttp.getHttpClient().newCall(request).enqueue(new FileCallBack(versionParams.getDownloadAPKPath(), context.getString(R.string.versionchecklib_download_apkname, context.getPackageName())) {
-
 
             @Override
             public void onSuccess(File file, Call call, Response response) {
                 listener.onCheckerDownloadSuccess(file);
-
             }
 
             @Override
             public void onDownloading(int progress) {
-                ALog.e("silent downloadProgress:" + progress + "");
                 int currentProgress = progress;
-//                showLoadingDialog(currentProgress);
                 if (currentProgress - lastProgress >= 5) {
                     lastProgress = currentProgress;
                 }
@@ -187,20 +171,17 @@ public class DownloadManager {
 
             @Override
             public void onDownloadFailed() {
-                ALog.e("file silent download failed");
-//                showFailDialog();
                 listener.onCheckerDownloadFail();
             }
+
         });
     }
 
     public static boolean checkAPKIsExists(Context context, String downloadPath) {
         return checkAPKIsExists(context, downloadPath, null);
-
     }
 
     /**
-     *
      * @param context
      * @param downloadPath
      * @param newestVersionCode 开发者认为的最新的版本号
@@ -212,24 +193,23 @@ public class DownloadManager {
         if (file.exists()) {
             try {
                 PackageManager pm = context.getPackageManager();
-                PackageInfo info = pm.getPackageArchiveInfo(downloadPath,
-                        PackageManager.GET_ACTIVITIES);
+                PackageInfo info = pm.getPackageArchiveInfo(downloadPath, PackageManager.GET_ACTIVITIES);
                 //判断安装包存在并且包名一样并且版本号不一样
-                ALog.e("本地安装包版本号：" + info.versionCode + "\n 当前app版本号：" + context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionCode);
                 if (context.getPackageName().equalsIgnoreCase(info.packageName) && context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionCode != info.versionCode) {
-                   //判断开发者传入的最新版本号是否大于缓存包的版本号，大于那么相当于没有缓存
+                    //判断开发者传入的最新版本号是否大于缓存包的版本号，大于那么相当于没有缓存
                     if (newestVersionCode != null && info.versionCode < newestVersionCode) {
                         result = false;
-                    } else
-                        result = true;
 
+                    } else {
+                        result = true;
+                    }
                 }
+
             } catch (Exception e) {
                 result = false;
             }
         }
         return result;
-
     }
 
     private static NotificationCompat.Builder createNotification(Context context) {
@@ -242,15 +222,10 @@ public class DownloadManager {
             notificationChannel.enableVibration(false);
             NotificationManager manager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
             manager.createNotificationChannel(notificationChannel);
-
-
         }
         builder = new NotificationCompat.Builder(context, CHANNEL_ID);
         builder.setAutoCancel(true);
-//        builder.setSmallIcon(R.mipmap.ic_launcher);
-//        builder.setOnlyAlertOnce(true);
         builder.setContentTitle(context.getString(R.string.app_name));
-//        builder.setSound(null);
 
         builder.setTicker(context.getString(R.string.versionchecklib_downloading));
         builder.setContentText(String.format(context.getString(R.string.versionchecklib_download_progress), 0));
@@ -261,4 +236,5 @@ public class DownloadManager {
 
         return builder;
     }
+
 }
--- library/src/main/java/com/allenliu/versionchecklib/v2/ui/UIActivity.java
+++ library/src/main/java/com/allenliu/versionchecklib/v2/ui/UIActivity.java
@@ -7,7 +7,6 @@ import android.support.v7.app.AlertDialog;
 import android.view.View;
 
 import com.allenliu.versionchecklib.R;
-import com.allenliu.versionchecklib.utils.ALog;
 import com.allenliu.versionchecklib.utils.AllenEventBusUtil;
 import com.allenliu.versionchecklib.utils.AppUtils;
 import com.allenliu.versionchecklib.v2.AllenVersionChecker;
@@ -19,64 +18,65 @@ import com.allenliu.versionchecklib.v2.eventbus.CommonEvent;
 import java.io.File;
 
 public class UIActivity extends AllenBaseActivity implements DialogInterface.OnCancelListener {
+
     private Dialog versionDialog;
-    private boolean isDestroy = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ALog.e("version activity create");
-
         showVersionDialog();
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        isDestroy = true;
-        ALog.e("version activity destroy");
-        super.onDestroy();
     }
 
     @Override
     public void receiveEvent(CommonEvent commonEvent) {
         switch (commonEvent.getEventType()) {
+
             case AllenEventType.SHOW_VERSION_DIALOG:
                 showVersionDialog();
                 break;
-        }
 
+            default:
+                break;
+
+        }
     }
 
     @Override
     public void showDefaultDialog() {
         if (getVersionBuilder() != null) {
             UIData uiData = getVersionBuilder().getVersionBundle();
-            String title = "提示", content = "检测到新版本";
+            String title = "提示";
+            String content = "检测到新版本";
+
             if (uiData != null) {
                 title = uiData.getTitle();
                 content = uiData.getContent();
             }
-            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this).setTitle(title).setMessage(content).setPositiveButton(getString(R.string.versionchecklib_confirm), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dealVersionDialogCommit();
 
-                }
-            });
+            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this)
+                    .setTitle(title)
+                    .setMessage(content)
+                    .setPositiveButton(getString(R.string.versionchecklib_confirm), new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dealVersionDialogCommit();
+                        }
+
+                    });
+
             if (getVersionBuilder().getForceUpdateListener() == null) {
                 alertBuilder.setNegativeButton(getString(R.string.versionchecklib_cancel), new DialogInterface.OnClickListener() {
+
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         onCancel(versionDialog);
                     }
+
                 });
-                alertBuilder.setCancelable(false);
-
-
-            } else {
-                alertBuilder.setCancelable(false);
             }
+            alertBuilder.setCancelable(false);
+
             versionDialog = alertBuilder.create();
             versionDialog.setCanceledOnTouchOutside(false);
             versionDialog.show();
@@ -86,83 +86,95 @@ public class UIActivity extends AllenBaseActivity implements DialogInterface.OnC
     @Override
     public void showCustomDialog() {
         if (getVersionBuilder() != null) {
-            ALog.e("show customization dialog");
-            versionDialog = getVersionBuilder().getCustomVersionDialogListener().getCustomVersionDialog(this, getVersionBuilder().getVersionBundle());
+            versionDialog = getVersionBuilder()
+                    .getCustomVersionDialogListener()
+                    .getCustomVersionDialog(this, getVersionBuilder().getVersionBundle());
+
+            versionDialog.show();
+
             try {
                 //自定义dialog，commit button 必须存在
                 final View view = versionDialog.findViewById(R.id.versionchecklib_version_dialog_commit);
                 if (view != null) {
-                    ALog.e("view not null");
-
                     view.setOnClickListener(new View.OnClickListener() {
+
                         @Override
                         public void onClick(View view) {
-                            ALog.e("click");
                             dealVersionDialogCommit();
                         }
+
                     });
 
                 } else {
                     throwWrongIdsException();
                 }
+
                 //如果有取消按钮，id也必须对应
                 View cancelView = versionDialog.findViewById(R.id.versionchecklib_version_dialog_cancel);
                 if (cancelView != null) {
                     cancelView.setOnClickListener(new View.OnClickListener() {
+
                         @Override
                         public void onClick(View view) {
                             onCancel(versionDialog);
                         }
+
                     });
                 }
+
             } catch (Exception e) {
                 e.printStackTrace();
                 throwWrongIdsException();
             }
-            versionDialog.show();
         }
     }
 
     private void showVersionDialog() {
         if (getVersionBuilder() != null && getVersionBuilder().getCustomVersionDialogListener() != null) {
             showCustomDialog();
+
         } else {
             showDefaultDialog();
         }
-        if (versionDialog != null)
+
+        if (versionDialog != null) {
             versionDialog.setOnCancelListener(this);
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (versionDialog != null && versionDialog.isShowing())
+        if (versionDialog != null && versionDialog.isShowing()) {
             versionDialog.dismiss();
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (versionDialog != null && !versionDialog.isShowing())
+        if (versionDialog != null && !versionDialog.isShowing()) {
             versionDialog.show();
+        }
     }
 
     private void dealVersionDialogCommit() {
         DownloadBuilder versionBuilder = getVersionBuilder();
         if (versionBuilder != null) {
+
             //如果是静默下载直接安装
             if (versionBuilder.isSilentDownload()) {
                 String downloadPath = versionBuilder.getDownloadAPKPath() + getString(R.string.versionchecklib_download_apkname, versionBuilder.getApkName() != null ? versionBuilder.getApkName() : getPackageName());
                 AppUtils.installApk(this, new File(downloadPath), versionBuilder.getCustomInstallListener());
                 checkForceUpdate();
-                //否定开始下载
+
             } else {
                 AllenEventBusUtil.sendEventBus(AllenEventType.START_DOWNLOAD_APK);
             }
+
             finish();
         }
     }
-
 
     @Override
     public void onCancel(DialogInterface dialogInterface) {
