1e9ef6adeeb79b795f87dd1ed6bad8103e232e8d
xukui <xukui@wondersgroup.com>
2019-06-27 18:01:27 +0800

    init

--- library/src/main/java/com/allenliu/versionchecklib/utils/AppUtils.java
+++ library/src/main/java/com/allenliu/versionchecklib/utils/AppUtils.java
@@ -13,28 +13,7 @@ import java.io.File;
 
 public final class AppUtils {
 
-    /**
-     * Don't let anyone instantiate this class.
-     */
     private AppUtils() {
-        throw new Error("Do not need instantiate!");
-    }
-
-    public static void installApk(Context context, File file) {
-        Intent intent = new Intent();
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.setAction(Intent.ACTION_VIEW);
-        Uri uri;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            uri = VersionFileProvider.getUriForFile(context, context.getPackageName() + ".versionProvider", file);
-            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        } else {
-            uri = Uri.fromFile(file);
-        }
-        intent.setDataAndType(uri,
-                "application/vnd.android.package-archive");
-        context.startActivity(intent);
-        AllenVersionChecker.getInstance().cancelAllMission();
     }
 
     public static void installApk(Context context, File file, CustomInstallListener listener) {
