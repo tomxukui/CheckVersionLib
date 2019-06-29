64c93f34212d7e8a25cce03c2c5f2b6e4214c406
xukui <152780382@qq.com>
2019-06-29 23:02:39 +0800

    init

--- library/src/main/java/com/allenliu/versionchecklib/v2/ui/VersionService.java
+++ library/src/main/java/com/allenliu/versionchecklib/v2/ui/VersionService.java
@@ -295,7 +295,9 @@ public class VersionService extends Service {
             }
             break;
 
-            case UpgradeEvent.CANCEL_UPGRADE: {//用户取消更新
+            case UpgradeEvent.CANCEL_UPGRADE://用户取消更新
+            case UpgradeEvent.CANCEL_DOWNLOADING: //用户取消下载
+            {
                 builderHelper.checkForceUpdate();
                 AllenVersionChecker.getInstance().cancelAllMission();
 
@@ -303,11 +305,6 @@ public class VersionService extends Service {
                 if (listener != null) {
                     listener.onCancel();
                 }
-            }
-            break;
-
-            case UpgradeEvent.CANCEL_DOWNLOADING: {//用户取消下载
-                AllenHttp.getHttpClient().dispatcher().cancelAll();
             }
             break;
 
